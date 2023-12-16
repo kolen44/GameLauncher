5f4c406d0e6facfd30adba9b8072e12a6844cbd9
Ilya Sukhan <malavov70@gmail.com>
2023-12-16 21:59:47 +0300

    fix: Menu components have been moved A Sidebar directory was created for the menu components and the corresponding elements were moved there (including layout and styles) to improve the quality of the project architecture.

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './styles.css'
+
+import HomeIcon from "../../assets/home.png"
+import Library from "../../assets/library.png"
+import Store from "../../assets/store.png"
+import Waitlist from "../../assets/waitlist.png"
+import Discount from "../../assets/discount.png"
+import Wallet from "../../assets/wallet.png"
+
+
+export default function Sidebar(){
+	return(
+		<aside className="sidebar">
+        <h1 className='sidebar__title'>QHOF</h1>
+        <div className="sidebar__divider"></div>
+        <ul className="sidebar__menu">
+          <li className="menu__item active">
+            <img className="item__icon" src={HomeIcon} />
+            <h1 className="item__text">Home</h1>
+          </li>
+          <li className="menu__item">
+            <img className="item__icon" src={Library} />
+            <h1 className="item__text">Library</h1>
+          </li>
+          <li className="menu__item">
+            <img className="item__icon" src={Store} />
+            <h1 className="item__text">Store</h1>
+          </li>
+          <li className="menu__item">
+            <img className="item__icon" src={Waitlist} />
+            <h1 className="item__text">Waitlist</h1>
+          </li>
+          <div className="sidebar__divider"></div>
+          <li className="menu__item">
+            <img className="item__icon" src={Discount} />
+            <h1 className="item__text">Discount</h1>
+          </li>
+          <li className="menu__item">
+            <img className="item__icon" src={Wallet} />
+            <h1 className="item__text">Wallet</h1>
+          </li>
+        </ul>
+      </aside>
+	)
+}
