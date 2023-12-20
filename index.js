637ecf7fcb15e79f356b62378caacb27d664f1ff
Ilya Sukhan <malavov70@gmail.com>
2023-12-20 20:34:57 +0300

    The backend branch has been created

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+fetch("https://api.thecatapi.com/v1/images/search") .then(
+    (response)=>
+     response.json()
+    )
+    .then ((data) => {
+        console.log(data)
+    })
+
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello WWW!' });
+});
+
+app.listen(3333, () => {
+    console.log('Application listening on port 3333!');
+});
